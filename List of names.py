f234faf10923fae1a2582c4c8c0ae75f2d4fcca9
mzimmer90 <max.zimmer@sky.uk>
2023-02-11 19:57:17 +0000

    Final annotations

--- List of names.py
+++ List of names.py
@@ -1,12 +1,27 @@
+# Initialize an empty list to store names
 names = []
+
+# Get user to enter amount of names to be added
 list_length = int(input("How many names would you like to add? "))
+
+# Loop for the amount of names
 for i in range(list_length):
-    name = input(f"Enter your {i+1} name: ")
+    # Get a name from the user and store it as a string
+    name = input(f"Enter your {i + 1} name: ")
+
+    # Check if the name contains only letters
     while not name.isalpha():
+        # If not, print an error message
         print("Please enter a name only containing letters.")
+        # Get the name again from the user
         name = input(f"Enter your {i + 1} name: ")
+
+    # Add the name to the list
     names.append(name)
 
+# Print the names separated by commas
 print("The names you entered were:", ', '.join(names))
+
+# Old code which printed each name individually in separate lines
 # for i in range(list_length):
 #     print(names[i])
--- Name Check.py
+++ Name Check.py
@@ -1,14 +1,29 @@
+# Get the name from the user and convert it to lowercase
+# This avoids issues with same letters but difference in capitalisation, i.e. H would be different from h
 name = input("Please enter a name: ").lower()
+
+# Get the length of the name
 name_length = len(name)
+
+# Print the length of the name
 print("Your name is", name_length, "characters long.")
 
+# Initialize an empty list to store repeating characters
 repeats = []
+
+# Loop through each character in the name
 for i in name:
+    # Check if the character repeats in the name
     if name.count(i) > 1:
+        # If the character repeats, check if it is already in the repeats list
         if i not in repeats:
+            # If not, add it to the repeats list
             repeats.append(i)
 
+# Check if there are any repeating characters
 if len(repeats) > 0:
+    # If there are, print the repeating characters separated by commas
     print("The following letters repeat:", ', '.join(repeats))
 else:
+    # If there are no repeating characters, print a message
     print("No letters repeat.")
